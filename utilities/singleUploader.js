1cc487baeb9dd56d913d69b67383e8c7fbac4d4a
SayedRazibur <sayedrazibur@gmail.com>
2024-09-16 14:32:41 +0600

    Initial commit

---
+++ utilities/singleUploader.js
@@ -0,0 +1,57 @@
+// external imports
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
+const createError = require("http-errors");
+
+function uploader(
+  subfolder_path,
+  allowed_file_types,
+  max_file_size,
+  error_msg
+) {
+  // File upload folder
+  const UPLOADS_FOLDER = `${__dirname}/../public/uploads/${subfolder_path}/`;
+
+  // define the storage
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      // :::::::::::::Create file:::::::::::::::::
+      fs.mkdir(UPLOADS_FOLDER, { recursive: true }, (err) => {
+        cb(null, UPLOADS_FOLDER);
+      });
+    },
+    filename: (req, file, cb) => {
+      const fileExt = path.extname(file.originalname);
+      const fileName =
+        file.originalname
+          .replace(fileExt, "")
+          .toLowerCase()
+          .split(" ")
+          .join("-") +
+        "-" +
+        Date.now();
+
+      cb(null, fileName + fileExt);
+    },
+  });
+
+  // preapre the final multer upload object
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: max_file_size,
+    },
+    fileFilter: (req, file, cb) => {
+      if (allowed_file_types.includes(file.mimetype)) {
+        cb(null, true);
+      } else {
+        cb(createError(error_msg));
+      }
+    },
+  });
+
+  return upload;
+}
+
+module.exports = uploader;
